8e47a84802ffb7ca2cb683093f71a0648ef5e4b8
owner1subtv <owner.1subtv@gmail.com>
2026-09-19 02:46:19 -0500

    Add coroutine support for Remote v2 pairing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ android {
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
